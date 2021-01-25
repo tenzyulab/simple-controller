feee1cfbcb4510c9342f8fdf16ded147bd17a1c2
tenzyu <tenzyu.on@gmail.com>
2021-01-25 23:51:10 +0900

    have the user confirm before purging all

--- cogs/manage_messages.py
+++ cogs/manage_messages.py
@@ -1,3 +1,5 @@
+from asyncio import TimeoutError
+
 from discord import Embed
 from discord.ext.commands import Cog, command
 
@@ -23,6 +25,23 @@ class ManageMessage(Cog):
     @command(name="purgeall", aliases=["pa"])
     async def purge_all_messages(self, ctx):
         """全てのメッセージを一括削除します。"""
+        await ctx.send(
+            f"""{ctx.author.mention} 本当に全てのメッセージを一括削除しますか？\n
+        実行する場合は20秒以内に `y` を送信してください。\n
+        それ以外のメッセージを送信するとキャンセルできます。"""
+        )
+        try:
+            response = await self.bot.wait_for(
+                "message",
+                timeout=20,
+                check=lambda messages: messages.author.id == ctx.author.id,
+            )
+        except TimeoutError:
+            await ctx.send("タイムアウトしました。")
+            return
+        if not response.content in ["y", "Y", "ｙ", "Ｙ"]:
+            await ctx.send("キャンセルしました。")
+            return
         await ctx.channel.purge(limit=None)
         embed = Embed(description="メッセージを全件削除しました。", colour=0x000000)
         embed.set_footer(text="このメッセージは10秒後に自動で削除されます。")
