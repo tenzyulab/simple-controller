cfce131c5860a33dbf5acaa5279b1e9500b9b6c4
tenzyu <tenzyu.on@gmail.com>
2021-01-26 20:54:41 +0900

    飽きた　よくわかりません

--- cogs/text_channel.py
+++ cogs/text_channel.py
@@ -1,30 +1,42 @@
 from asyncio import TimeoutError
 
 from discord import Embed
-from discord.ext.commands import Cog, command
+from discord.ext.commands import Cog, check, command, group, has_permissions
 
 
-class ManageMessage(Cog):
+class TextChannel(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def cog_check(self, ctx):
-        if ctx.author.permissions_in(ctx.channel).manage_messages:
-            return True
-        await ctx.send("あなたにはメッセージの管理権限がありません。")
-        return False
+    async def has_permission_manage_messages(self, ctx):
+        return ctx.author.permissions_in(ctx.channel).manage_messages
 
-    @command(name="purge", aliases=["p"])
-    async def purge_messages(self, ctx, number: int):
+    @group()
+    async def channel(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("無効なchannelコマンドです。")
+
+    @channel.command()
+    async def purge(self, ctx, number: int):
         """!purge <number> で指定された数のメッセージを一括削除します。"""
-        await ctx.channel.purge(limit=num + 1)
+        if not await self.has_permission_manage_messages(ctx):
+            await ctx.send("あなたにはメッセージの管理権限がありません。")
+            return
+        await ctx.channel.purge(limit=number + 1)
         embed = Embed(description=f"メッセージを{number}件削除しました。", colour=0x000000)
         embed.set_footer(text="このメッセージは10秒後に自動で削除されます。")
         await ctx.send(embed=embed, delete_after=10)
 
-    @command(name="purgeall", aliases=["pa"])
-    async def purge_all_messages(self, ctx):
+    @command(name="purge", aliases=["p", "cp"])
+    async def _purge(self, ctx, number):
+        await self.purge(ctx, number)
+
+    @channel.command()
+    async def purgeall(self, ctx):
         """全てのメッセージを一括削除します。"""
+        if not await self.has_permission_manage_messages(ctx):
+            await ctx.send("あなたにはメッセージの管理権限がありません。")
+            return
         await ctx.send(
             f"""{ctx.author.mention} 本当に全てのメッセージを一括削除しますか？\n
         実行する場合は20秒以内に `y` を送信してください。\n
@@ -47,6 +59,10 @@ class ManageMessage(Cog):
         embed.set_footer(text="このメッセージは10秒後に自動で削除されます。")
         await ctx.send(embed=embed, delete_after=10)
 
+    @command(name="purgeall", aliases=["pa", "cpa"])
+    async def _purgeall(self, ctx):
+        await self.purgeall(ctx)
+
 
 def setup(bot):
-    bot.add_cog(ManageMessage(bot))
+    bot.add_cog(TextChannel(bot))
