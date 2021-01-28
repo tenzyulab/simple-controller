8e27e6e19a08892526d514efeb0bfa217a6f9dbe
tenzyu <tenzyu.on@gmail.com>
2021-01-28 16:17:48 +0900

    added channel delete command

--- cogs/text_channel.py
+++ cogs/text_channel.py
@@ -1,18 +1,56 @@
 from asyncio import TimeoutError
+from textwrap import dedent
 
 from discord import Embed
-from discord.ext.commands import Cog, command, group, has_permissions
+from discord.ext.commands import Cog, Context, command, group, has_permissions
 
 
 class TextChannel(Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def confirm_dialog(self, ctx, what: str):
+        message = dedent(
+            f"""
+            {ctx.author.mention} 本当に{what}しますか?
+            実行する場合は20秒以内に `y` を送信してください。
+            それ以外のメッセージを送信するとキャンセルできます。
+            """
+        )
+        await ctx.send(message)
+
+    async def get_confirmation_response(self, ctx):
+        try:
+            response = await self.bot.wait_for(
+                "message",
+                timeout=20,
+                check=lambda messages: messages.author.id == ctx.author.id,
+            )
+            return response
+        except TimeoutError:
+            return None
+
     @group()
     async def channel(self, ctx):
         """help channelで詳細を表示"""
         if not ctx.invoked_subcommand:
             await ctx.send("サブコマンドを指定してください。")
+
+    @channel.command(name="delete", aliases=["del"])
+    @has_permissions(manage_channels=True)
+    async def _delete(self, ctx: Context, *, reason: str = None):
+        """チャンネルを削除します。"""
+        if reason is None:
+            reason = "削除された理由は記載されていません。"
+        await self.confirm_dialog(ctx, "チャンネルを削除")
+        response = await self.get_confirmation_response(ctx)
+        if response is None:
+            await ctx.send("タイムアウトしました。")
+            return
+        if not response.content in ["y", "Y", "ｙ", "Ｙ"]:
+            await ctx.send("キャンセルしました。")
+            return
+        await ctx.channel.delete(reason=reason)
 
     @channel.command()
     @has_permissions(manage_messages=True)
@@ -27,18 +65,9 @@ class TextChannel(Cog):
     @has_permissions(manage_messages=True)
     async def purgeall(self, ctx):
         """全てのメッセージを一括削除します。"""
-        await ctx.send(
-            f"""{ctx.author.mention} 本当に全てのメッセージを一括削除しますか？\n
-        実行する場合は20秒以内に `y` を送信してください。\n
-        それ以外のメッセージを送信するとキャンセルできます。"""
-        )
-        try:
-            response = await self.bot.wait_for(
-                "message",
-                timeout=20,
-                check=lambda messages: messages.author.id == ctx.author.id,
-            )
-        except TimeoutError:
+        await self.confirm_dialog(ctx, "全てのメッセージを削除")
+        response = await self.get_confirmation_response(ctx)
+        if response is None:
             await ctx.send("タイムアウトしました。")
             return
         if not response.content in ["y", "Y", "ｙ", "Ｙ"]:
