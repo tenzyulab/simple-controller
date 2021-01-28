45c0dc5b709f5fe2eb1cdd1fc0c05efd4280e5c3
tenzyu <tenzyu.on@gmail.com>
2021-01-28 12:40:21 +0900

    sorted in the same order as Discord settings

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -3,24 +3,24 @@ from discord.ext.commands import Bot, Cog, CommandError, Context, MissingPermiss
 
 class ErrorHandler(Cog):
     permissions_jp = {
-        "administrator": "管理者",
-        "create_instant_invite": "招待を作成",
         "manage_channels": "チャンネルの管理",
         "manage_roles": "権限の管理",
+        "create_instant_invite": "招待を作成",
         "send_messages": "メッセージを送信",
-        "manage_messages": "メッセージの管理",
         "embed_links": "埋め込みリンク",
         "attach_files": "ファイルを添付",
-        "read_message_history": "メッセージ履歴を読む",
-        "external_emojis": "外部の絵文字の使用",
         "add_reactions": "リアクションの追加",
+        "external_emojis": "外部の絵文字の使用",
+        "manage_messages": "メッセージの管理",
+        "read_message_history": "メッセージ履歴を読む",
         "connect": "接続",
         "speak": "発言",
+        "use_voice_activation": "音声検出を使用",
+        "priority_speaker": "プライオリティスピーカー",
         "mute_members": "メンバーをミュート",
         "deafen_members": "メンバーのスピーカーをミュート",
         "move_members": "メンバーを移動",
-        "use_voice_activation": "音声検出を使用",
-        "priority_speaker": "プライオリティスピーカー",
+        "administrator": "管理者",
     }
 
     def __init__(self, bot):
