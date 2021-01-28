a513ccbce015381e60b4b9dcb02ccb8383f91ab4
AttoCat <wazasiyo2525a@gmail.com>
2021-01-28 17:08:33 +0900

    :hammer: fix KeyError and add annotations

--- src/custom_prefix.py
+++ src/custom_prefix.py
@@ -1,6 +1,6 @@
 import json
-from re import compile
-from typing import Dict, List, Tuple
+from re import Match, compile
+from typing import Dict, List, Optional, Tuple
 
 import aiofiles
 import const
@@ -16,25 +16,30 @@ with open("database/prefix.json", encoding="UTF-8") as f:
 
 def get_prefix(bot: Bot, message: Message) -> List[str]:
     prefixes = []
-    match_nickname = NICKNAME_PATTERN.match(message.guild.me.display_name)
+    match_nickname: Optional[Match] = NICKNAME_PATTERN.match(
+        message.guild.me.display_name
+    )
     if match_nickname:
         prefixes.append(match_nickname.group(1))
     if str(message.guild.id) in prefix_dict.keys():
         prefixes.append(prefix_dict[str(message.guild.id)])
     if not prefixes:
-        prefixes = [const.BOT_PREFIX]
+        prefixes: List[str] = [const.BOT_PREFIX]
     return when_mentioned_or(*prefixes)(bot, message)
 
 
 async def change_prefix(guild_id: int, new_prefix: str) -> Tuple[str]:
-    before_prefix: str = prefix_dict[str(guild_id)]
+    try:
+        before_prefix: str = prefix_dict[str(guild_id)]
+    except KeyError:
+        before_prefix: str = const.BOT_PREFIX
     prefix_dict[str(guild_id)] = new_prefix
     await reload_prefix()
     return before_prefix, new_prefix
 
 
 async def delete_prefix(guild_id: int) -> None:
-    before_prefix = prefix_dict[str(guild_id)]
+    before_prefix: str = prefix_dict[str(guild_id)]
     del prefix_dict[str(guild_id)]
     await reload_prefix()
     return before_prefix
