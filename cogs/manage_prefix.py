bc2b1b6d12034c9418a2e4b7706501bc54539dc1
tenzyu <tenzyu.on@gmail.com>
2021-01-28 18:24:55 +0900

    added help message

--- cogs/manage_prefix.py
+++ cogs/manage_prefix.py
@@ -11,15 +11,19 @@ class ManagePrefix(Cog):
     @has_permissions(manage_nicknames=True)
     @group(invoke_without_command=True, aliases=["p"])
     async def prefix(self, ctx: Context) -> None:
+        """help prefixで詳細を表示"""
         now_prefix: List[str] = get_prefix(self.bot, ctx.message)
         await ctx.send(f"現在のプレフィックスは {' と '.join(now_prefix[2:])} です。")
 
     @prefix.command(name="set", aliases=["s"])
+    async def _set(self, ctx: Context, new_prefix: str) -> None:
+        """プレフィックスを変更します。"""
         result: Tuple[str] = await change_prefix(ctx.guild.id, new_prefix)
         await ctx.send("プレフィックスを {} から {} に変更しました。".format(*result))
 
     @prefix.command(aliases=["r"])
     async def reset(self, ctx: Context) -> None:
+        """カスタムプレフィックスを削除します。"""
         before_prefix: str = await delete_prefix(ctx.guild.id)
         await ctx.send(
             f"カスタムプレフィックス {before_prefix} を削除しました。\nニックネームでプレフィックスを指定している場合はそちらが優先されます。"
