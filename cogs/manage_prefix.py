1a684e4495cf14c78f71f50d5b25a10b2082cadb
AttoCat <wazasiyo2525a@gmail.com>
2021-01-28 17:03:42 +0900

    :hammer: add aliases

--- cogs/manage_prefix.py
+++ cogs/manage_prefix.py
@@ -11,17 +11,17 @@ class ManagePrefix(Cog):
     async def cog_check(self, ctx: Context) -> bool:
         return ctx.author.guild_permissions.manage_nicknames
 
-    @group(invoke_without_command=True)
+    @group(invoke_without_command=True, aliases=["p"])
     async def prefix(self, ctx: Context) -> None:
         now_prefix: List[str] = get_prefix(self.bot, ctx.message)
         await ctx.send(f"現在のプレフィックスは {' と '.join(now_prefix[2:])} です。")
 
-    @prefix.command()
+    @prefix.command(aliases=["s"])
     async def set(self, ctx: Context, new_prefix: str) -> None:
         result: List[str] = await change_prefix(ctx.guild.id, new_prefix)
         await ctx.send("プレフィックスを {} から {} に変更しました。".format(*result))
 
-    @prefix.command()
+    @prefix.command(alias=["r"])
     async def reset(self, ctx: Context) -> None:
         before_prefix: str = await delete_prefix(ctx.guild.id)
         await ctx.send(
