826840862ddd200246ab5744d9e1beb0fdc1130d
tenzyu <tenzyu.on@gmail.com>
2021-03-18 17:47:02 +0900

    added bot invite command

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -1,6 +1,6 @@
 from time import monotonic
 
-from discord.ext.commands import Bot, Cog, command
+from discord.ext.commands import Bot, Cog, Context, command
 
 
 class Utilities(Cog):
@@ -14,6 +14,12 @@ class Utilities(Cog):
         msg = await ctx.send("計算中...")
         latency = (monotonic() - tmp) * 1000
         await msg.edit(content=f"Pong! 応答速度は **{int(latency)}** ms です。")
+
+    @command(aliases=["in"])
+    async def invite(self, ctx: Context):
+        """この BOT の招待リンクを送ります。"""
+        url = "https://discord.com/api/oauth2/authorize?client_id=802867226700677120&permissions=8&scope=bot"
+        await ctx.reply(url)
 
 
 def setup(bot: Bot):
